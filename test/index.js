8011dc78848ba8c77dd645d8eabc0da3d01b8cba
Igor Morales Pereira <igor.pereira@marttech.com.br>
2021-06-09 02:26:43 -0300

    logger

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import { Logger, setLevel } from '../lib/index.js';
 
-console.log(new Logger(), new Logger())
+console.log(new Logger())
 console.log(setLevel)
 setLevel('info');
-console.log(window[Symbol.for(__main_logger_private_access_key)].level)
+console.log(window[Symbol.for(__main_logger_private_access_key)].__proto__)
+console.log(Logger.prototype)
