4ecb67612e1e0b9b6b76ac87afc1aaba65109bfb
Igor Morales Pereira <igor@gargantas.org>
2021-06-25 15:17:32 -0300

    prop descriptors and prototype

--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,8 +1,20 @@
 export type PropKey = string | number | symbol;
- 
-export function InvalidConstructorError(func: Function): SyntaxError;
-export function InvalidConstructorError(name: string): SyntaxError;
-export function InvalidConstructorError(symbol: symbol): SyntaxError;
-export function InvalidConstructorError(object: object): SyntaxError;
+
+/**
+ * Does not fully work as a class.
+ * 
+ * TODO: support both function and class notation in future
+ */
+export class InvalidConstructorError extends SyntaxError {
+  constructor(func: Function);
+  constructor(name: string);
+  constructor(symbol: symbol);
+  constructor(object: object);
+
+  override name: string;
+  override toString(): string;
+}
 
 export function ToBoolean(x: unknown): boolean;
+
+export function MakeDefaultObjectDescriptor(obj: unknown, options?: PropertyDescriptor | null, usePropAcessors?: boolean): PropertyDescriptorMap;
